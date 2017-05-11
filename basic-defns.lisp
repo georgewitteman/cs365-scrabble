1ede3faac335efea5ddc097913aa0a003d8dfb7f
George Witteman <georgewitteman@gmail.com>
2017-05-11 11:31:35 -0400

    Mostly working do-move!

--- basic-defns.lisp
+++ basic-defns.lisp
@@ -1,13 +1,10 @@
-
 ;; =====================
 ;; Basic Definitions
 ;; =====================
 
-
-
 (defparameter *files*
-    (list "basic-defns"
-	  "play-scr"))
+  (list "basic-defns"
+        "play-scrabble"))
 
 ;; CL
 ;; ----------------------
@@ -32,47 +29,60 @@
 
 (defconstant *num-tiles-left* 100)
 
-
-
 (defconstant *letters-array* 
-    (make-array 27 :initial-contents
-		'(- A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)))
+             (make-array 27 :initial-contents
+                         '(- A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)))
 
 (defconstant *initial-tiles-left-array* 
-    (make-array 27 :initial-contents 
-		;;- A B C D E  F G H I J K L M N O P Q R S T U V W X Y Z
-    		'(2 9 2 2 4 12 2 3 2 9 1 1 4 2 6 8 2 1 6 4 6 4 2 2 1 2 1)))
+             (make-array 27 :initial-contents 
+                         ;;- A B C D E  F G H I J K L M N O P Q R S T U V W X Y Z
+                         '(2 9 2 2 4 12 2 3 2 9 1 1 4 2 6 8 2 1 6 4 6 4 2 2 1 2 1)))
 
 (defconstant *letter-val-array*
-    (make-array 27 :initial-contents
-		'(0 1 3 3 2 1 4 2 4 1 8 5 1 3 1 1 3 10 1 1 1 1 4 4 8 4 10)))
+             (make-array 27 :initial-contents
+                         '(0 1 3 3 2 1 4 2 4 1 8 5 1 3 1 1 3 10 1 1 1 1 4 4 8 4 10)))
 
 (defconstant *initial-board*
-    (make-array '(15 15) :initial-contents
-		'((tw -- -- dl -- -- -- tw -- -- -- dl -- -- tw)
-		  (-- dw -- -- -- tl -- -- -- tl -- -- -- dw --)
-		  (-- -- dw -- -- -- dl -- dl -- -- -- dw -- --)
-		  (dl -- -- dw -- -- -- dl -- -- -- dw -- -- dl)
-		  (-- -- -- -- dw -- -- -- -- -- dw -- -- -- --)
-		  (-- tl -- -- -- tl -- -- -- tl -- -- -- tl --)
-		  (-- -- dl -- -- -- dl -- dl -- -- -- dl -- --)
-		  (tw -- -- dl -- -- -- dw -- -- -- dl -- -- tw)
-		  (-- -- dl -- -- -- dl -- dl -- -- -- dl -- --)
-		  (-- tl -- -- -- tl -- -- -- tl -- -- -- tl --)
-		  (-- -- -- -- dw -- -- -- -- -- dw -- -- -- --)
-		  (dl -- -- dw -- -- -- dl -- -- -- dw -- -- dl)
-		  (-- -- dw -- -- -- dl -- dl -- -- -- dw -- --)
-		  (-- dw -- -- -- tl -- -- -- tl -- -- -- dw --)
-		  (tw -- -- dl -- -- -- tw -- -- -- dl -- -- tw))))
+             (make-array '(15 15) :initial-contents
+                         '((tw -- -- dl -- -- -- tw -- -- -- dl -- -- tw)
+                           (-- dw -- -- -- tl -- -- -- tl -- -- -- dw --)
+                           (-- -- dw -- -- -- dl -- dl -- -- -- dw -- --)
+                           (dl -- -- dw -- -- -- dl -- -- -- dw -- -- dl)
+                           (-- -- -- -- dw -- -- -- -- -- dw -- -- -- --)
+                           (-- tl -- -- -- tl -- -- -- tl -- -- -- tl --)
+                           (-- -- dl -- -- -- dl -- dl -- -- -- dl -- --)
+                           (tw -- -- dl -- -- -- dw -- -- -- dl -- -- tw)
+                           (-- -- dl -- -- -- dl -- dl -- -- -- dl -- --)
+                           (-- tl -- -- -- tl -- -- -- tl -- -- -- tl --)
+                           (-- -- -- -- dw -- -- -- -- -- dw -- -- -- --)
+                           (dl -- -- dw -- -- -- dl -- -- -- dw -- -- dl)
+                           (-- -- dw -- -- -- dl -- dl -- -- -- dw -- --)
+                           (-- dw -- -- -- tl -- -- -- tl -- -- -- dw --)
+                           (tw -- -- dl -- -- -- tw -- -- -- dl -- -- tw))))
 
 ;;  The TILE struct
 ;; -----------------------
 
 (defstruct (tile (:print-function print-tile))
-  letter
-  value
-  row
-  col)
+  letter ; CHARACTER, the letter on the tile (- for blank)
+  value  ; integer value of the tile
+  row    ; position of the tile, NIL if not yet on the board
+  col    ; "
+  )
+
+;;  The SCRABBLE struct
+;; ---------------------
+
+(defstruct (scrabble (:print-function print-scrabble))
+  board          ; a 15x15 array
+  whose-turn     ; *ply0* or *ply1*
+  rack_0         ; a LIST of TILEs 
+  rack_1         ; "
+  bag            ; a LIST of TILEs representing the tiles left in the bag
+  num-tiles-left ; number of tiles left in the bag
+  score          ; a vector containing each players score
+  )
+
 
 ;; PRINT-TILE
 ;; -------------------------
@@ -86,28 +96,7 @@
 
 (defun tile-eq? (tile_1 tile_2)
   (equal (tile-letter tile_1)
-	 (tile-letter tile_2)))
-
-
-;;  The SCRABBLE struct
-;; ---------------------
-
-(defstruct (scrabble (:print-function print-scrabble))
-  ;; BOARD: a 15x15 array of letters
-  board
-  ;; WHOSE-TURN
-  whose-turn
-  ;; RACK_0,1: list of tiles
-  rack_0
-  rack_1
-  ;; BAG: a list of tiles
-  bag
-  ;; LETTER-VAL: a 27 vector of integers
-  (point-values *letter-val-array*)
-  ;; NUM-TILES-LEFT: integer
-  num-tiles-left
-  ;; SCORE: a 2 vector of integers
-  score)
+         (tile-letter tile_2)))
 
 
 ;; MAKE-BAG
@@ -119,10 +108,10 @@
   (let ((bag '()))
     (dotimes (i 27 bag)
       (dotimes (j (svref *initial-tiles-left-array* i))
-	(setf bag (cons (make-tile :letter (svref *letters-array* i)
-				   :value (svref *letter-val-array* i))
-			bag))))))
-	
+        (setf bag (cons (make-tile :letter (svref *letters-array* i)
+                                   :value (svref *letter-val-array* i))
+                        bag))))))
+
 
 ;; WHOSE-TURN
 ;; -----------------
@@ -140,13 +129,13 @@
 
 (defun copy-array (arr)
   (let* ((dim (array-dimensions arr))
-	 (copy (make-array dim)))
+         (copy (make-array dim)))
     (if (= 1 (list-length dim)
-	   (dotimes (x (first dim) copy)
-	     (setf (svref copy x) (svref arr x))))
-	(dotimes (r (first dim))
-	  (dotimes (c (second dim) copy)
-	    (setf (aref copy r c) (aref arr r c)))))))
+           (dotimes (x (first dim) copy)
+             (setf (svref copy x) (svref arr x))))
+      (dotimes (r (first dim))
+        (dotimes (c (second dim) copy)
+          (setf (aref copy r c) (aref arr r c)))))))
 
 ;; COPY-GAME
 ;; -----------------
@@ -155,13 +144,12 @@
 
 (defun copy-game (game)
   (make-scrabble :board (copy-array (scrabble-board game))
-		 :whose-turn (scrabble-whose-turn game)
-		 :rack_0 (copy-list (scrabble-rack_0 game))
-		 :rack_1 (copy-list (scrabble-rack_1 game))
-		 :bag (copy-list (scrabble-bag game))
-		 :num-tiles-left (scrabble-tiles-left game)
-		 :point-values (copy-array (scrabble-point-values game))
-		 :score (copy-array (scrabble-score game))))
+                 :whose-turn (scrabble-whose-turn game)
+                 :rack_0 (copy-list (scrabble-rack_0 game))
+                 :rack_1 (copy-list (scrabble-rack_1 game))
+                 :bag (copy-list (scrabble-bag game))
+                 :num-tiles-left (scrabble-tiles-left game)
+                 :score (copy-array (scrabble-score game))))
 
 ;; PRINT-SCRABBLE
 ;; -----------------
@@ -171,54 +159,56 @@
 (defun print-scrabble (game str d)
   (declare (ignore d))
   (let* ((board (scrabble-board game))
-	 (rack (scrabble-rack game))
-	 (p (whose-turn game))
-	 (point-val (scrabble-point-values game))
-	 (score (scrabble-score game))
-	 (bag (scrabble-bag game)))
-    
+         (p (whose-turn game))
+         (score (scrabble-score game))
+         (bag (scrabble-bag game)))
+
     ;; Print Title
     (format str "~%                   Scrabble~%~%")
-    
-    
+
+
     ;; Print Board
     (dotimes (i 15)
       (dotimes (j 15)
-	(format str "~A "(aref board i j)))
+        (format str "~A "(aref board i j)))
       (format str "~%"))
-      
-    
+
+
     (format str "~% ~%")
-    
+
     ;; Print Player 1 and Player 2 Rack
-    (format str "Player 1                        Player 2    ~%")
-    
-      (dolist (tile (scrabble-rack_0 game))
-	(if (equal *ply0* p)
-	    (print-tile tile str d)
-	  (format str "- ")))
-      (format str "                ")
-      (dolist (tile (scrabble-rack_1 game))
-	(if (equal *ply1* p)
-	    (print-tile tile str d)
-	  (format str "- ")))
-    
+    (format str "Player 1                      Player 2    ~%")
+
+    (format str "Rack: ")
+    (dolist (tile (scrabble-rack_0 game))
+      (if (equal *ply0* p)
+        (print-tile tile str d)
+        (format str "- ")))
+    (format str "          Rack: ")
+    (dolist (tile (scrabble-rack_1 game))
+      (if (equal *ply1* p)
+        (print-tile tile str d)
+        (format str "- ")))
+
+    (format str "~%Score: ~A                      Score: ~A"
+            (svref (scrabble-score game) 0)
+            (svref (scrabble-score game) 1))
+
     (format str "~% ~% ~%")
-    
+
     (let ((letter 0)
-	  (val 0)
-	  (rem 0))
+          (val 0)
+          (rem 0))
       (format str "Letters: ")
       (dotimes (i 27)
-	(setf letter (svref *letters-array* i)) 
-	(format str "~A  " letter))
+        (setf letter (svref *letters-array* i)) 
+        (format str "~A  " letter))
       (format str "~%Values:  ")
       (dotimes (i 27)
-	(setf val (svref *letter-val-array* i))
-	(format str "~A " val)
-	(when (< val 10) (format str " ")))
+        (setf val (svref *letter-val-array* i))
+        (format str "~A " val)
+        (when (< val 10) (format str " ")))
       (format str "~%"))))
-     
 
 ;; NEW-SCRABBLE
 ;; ----------------
@@ -227,13 +217,13 @@
 
 (defun new-scrabble ()
   (let ((game (make-scrabble
-	       :board *initial-board*
-	       :whose-turn *ply0*
-	       :bag (make-bag)
-	       :num-tiles-left *num-tiles-left*
-	       :rack_0 ()
-	       :rack_1 ()
-	       :score (make-array 2 :initial-element 0))))
+                :board *initial-board*
+                :whose-turn *ply0*
+                :bag (make-bag)
+                :num-tiles-left *num-tiles-left*
+                :rack_0 ()
+                :rack_1 ()
+                :score (make-array 2 :initial-element 0))))
     (shake-bag! game)
     (pick-tiles! game *ply0* 7)
     (pick-tiles! game *ply1* 7)
@@ -250,15 +240,15 @@
 
 (defun nth-elt-index-acc (arr n i acc)
   (cond
-   ;; BASE CASE 1: N = 0
-   ((= n 0)
-    i)
-   ;; BASE CASE 2: ACC = 0
-   ((<= acc 0)
-    (decf i))
-   ;; RECURSIVE CASE: ACC < N
-   (T
-    (nth-elt-index-acc arr n (incf i) (- acc (svref arr (- i 1)))))))
+    ;; BASE CASE 1: N = 0
+    ((= n 0)
+     i)
+    ;; BASE CASE 2: ACC = 0
+    ((<= acc 0)
+     (decf i))
+    ;; RECURSIVE CASE: ACC < N
+    (T
+      (nth-elt-index-acc arr n (incf i) (- acc (svref arr (- i 1)))))))
 
 ;; NTH-ELT-INDEX
 ;; -----------------
@@ -277,21 +267,17 @@
 
 (defun shake-bag! (game)
   (setf (scrabble-bag game)
-    (shake-bag-acc (scrabble-bag game) ())))
+        (shake-bag-acc (scrabble-bag game) ())))
 
 (defun shake-bag-acc (bag new-bag)
   (cond
-   ;; BASE CASE: BAG is empty
-   ((null bag)
-    new-bag)
-   ;; RECURSIVE CASE: BAG is nonempty
-   (T
-    (let* ((length (list-length bag))
-	   (rand (random length))
-	   (tile (nth rand bag)))
-      (shake-bag-acc (remove tile bag :count 1)
-		     (cons tile new-bag))))))
-
-
-
-  
+    ;; BASE CASE: BAG is empty
+    ((null bag)
+     new-bag)
+    ;; RECURSIVE CASE: BAG is nonempty
+    (T
+      (let* ((length (list-length bag))
+             (rand (random length))
+             (tile (nth rand bag)))
+        (shake-bag-acc (remove tile bag :count 1)
+                       (cons tile new-bag))))))
---
+++ play-scrabble.lisp
@@ -0,0 +1,319 @@
+;; ========================
+;; PLAY-SCR.LISP
+;; ========================
+
+;;  DO-MOVE!
+;; ----------------------
+;;  INPUTS: GAME, a SCRABBLE struct
+;;          TILES, a LIST of TILE structs
+;;          LOCS, a LIST of LISTs of 2 integers representing the row/col
+;;          locations for their corresponding TILEs
+;;  OUTPUTS: The score for the given move
+;;  SIDE-EFFECT: Modifies GAME to include TILES on the board and modifies
+;;               each TILE to include it's position
+
+(defun do-move! (game tiles locs)
+  (place-all-tiles! (scrabble-board game) tiles locs)
+  (refill-racks! game)
+  (let ((score (score (scrabble-board game) tiles)))
+    (setf (svref (scrabble-score game) (whose-turn game)) score)))
+
+(defun place-all-tiles! (board tiles locs)
+  (cond ((null tiles) board)
+        (t (place-tile! board
+                        (first tiles)
+                        (first (first locs))
+                        (second (first locs)))
+           (place-all-tiles! board (rest tiles) (rest locs)))))
+
+;;  SCORE
+;; ------------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board. Note that
+;;                 the NEW-TILES should already be placed on this board
+;;          NEW-TILES, a list of TILE structs representing tiles
+;;                     on the board
+;;  OUTPUT: The score for the given move
+
+(defun score (board new-tiles)
+  (score-words board (get-new-words board new-tiles)))
+
+;;  SCORE-WORDS
+;; --------------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board
+;;          WORDS, A LIST containing a LIST of TILEs representing words
+;;  OUTPUT: The total score for all the given words
+
+(defun score-words (board words)
+  (score-words-acc board words 0))
+
+(defun score-words-acc (board words score)
+  (cond ((null words) score)
+        (t (score-words-acc board
+                            (rest words)
+                            (+ (* (score-word board (first words))
+                                  (get-word-multiplier (first words)))
+                               score)))))
+
+;;  SCORE-WORD
+;; ----------------------------
+;;  INPUTS: BOARD, a 2D array represnting a scrabble board
+;;          WORD, a LIST of TILEs representing a word
+;;  OUTPUT: The score for the given word
+
+(defun score-word (board word)
+  (format t "score for ~A:~A ~%" word (score-word-acc board word 0))
+  (score-word-acc board word 0))
+
+(defun score-word-acc (board word score)
+  (cond ((null word) score)
+        (t (score-word-acc board
+                           (rest word)
+                           (+ score
+                              (* (get-tile-multiplier (tile-row (first word))
+                                                      (tile-col (first word)))
+                                 (tile-value (first word))))))))
+
+;;  GET-TILE-MULTIPLIER
+;; ------------------------
+;;  INPUTS: ROW & COL, position of space on the board
+;;  OUTPUT: The multiplier for that space
+
+(defun get-tile-multiplier (row col)
+  (let ((premium (get-space-premium row col)))
+    (cond ((equal premium *dl*) 2)
+          ((equal premium *tl*) 3)
+          (t 1))))
+
+;;  GET-WORD-MULTIPLIER
+;; ------------------------
+;;  INPUTS: WORD, A LIST of TILEs representing a word on the board
+;;  OUTPUT: The multiplier for that word. For example, a word with a tile
+;;          in (0 0) would return at least 3 (possibly 9+ if it covers another
+;;          triple word space.
+
+(defun get-word-multiplier (word)
+  (get-word-multiplier-acc word 1))
+
+(defun get-word-multiplier-acc (word acc)
+  (if (null word)
+    acc
+    (let* ((row (tile-row (first word)))
+           (col (tile-col (first word)))
+           (premium (get-space-premium row col)))
+      (cond ((equal premium *dw*)
+             (get-word-multiplier-acc (rest word)
+                                      (* acc 2)))
+            ((equal premium *tw*)
+             (get-word-multiplier-acc (rest word)
+                                      (* acc 3)))
+            (t (get-word-multiplier-acc (rest word) acc))))))
+
+;;  GET-SPACE-PREMIUM
+;; ------------------------------
+;;  INPUTS: ROW, COL, the row and col of the space to check
+;;  OUTPUT: The premium for that given space, NIL if no premium
+
+(defun get-space-premium (row col)
+  (let ((loc (aref *initial-board* row col)))
+    (if (equal loc *open*)
+      nil
+      loc)))
+
+;;  GET-NEW-WORDS
+;; --------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board
+;;          NEW-TILES, a LIST of TILEs representing the newly placed tiles
+;;  OUTPUTS: A LIST containing a LIST of TILEs representing all the newly
+;;           created words
+
+(defun get-new-words (board new-tiles)
+  (get-new-words-acc board new-tiles nil))
+
+(defun get-new-words-acc (board new-tiles acc)
+  (cond ((null new-tiles) (remove-duplicates acc :test #'equal))
+        (t (get-new-words-acc
+             board
+             (rest new-tiles)
+             (append acc
+                     (get-words-at-tile board (first new-tiles)))))))
+
+;;  GET-WORDS-AT-TILE
+;; -----------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board
+;;          TILE, a TILE on the BOARD
+;;  OUTPUT: A LIST of LISTs of TILEs representing all words that are made
+;;          from the given TILE, NIL if no words are made
+
+(defun get-words-at-tile (board tile)
+  (let ((words nil)
+        (horizontal (get-word-from-start-end
+                      board
+                      (get-end-tile board tile -1 0)
+                      (get-end-tile board tile 1 0)))
+        (vertical (get-word-from-start-end
+                    board
+                    (get-end-tile board tile 0 -1)
+                    (get-end-tile board tile 0 1))))
+    (if (> (length horizontal) 1)
+      (setf words (append words (list horizontal))))
+    (if (> (length vertical) 1)
+      (setf words (append words (list vertical))))
+    words))
+
+;;  GET-WORD-FROM-START-END
+;; ------------------------------
+;;  INPUTS: START-TILE, a TILE that begins a word
+;;          END-TILE, a TILE that ends a word
+;;  OUTPUT: A LIST of all the TILEs from START-TILE to END-TILE inclusive
+
+(defun get-word-from-start-end (board start-tile end-tile)
+  (let ((start-row (tile-row start-tile))
+        (start-col (tile-col start-tile))
+        (end-row (tile-row end-tile))
+        (end-col (tile-col end-tile))
+        (locs nil))
+    (if (equal start-row end-row)
+      ;; Horizontal
+      (while (<= start-col end-col)
+             (setf locs (cons (tile-from-loc board start-row start-col)
+                              locs))
+             (incf start-col))
+      ;; Vertical
+      (while (<= start-row end-row)
+             (setf locs (cons (tile-from-loc board start-row start-col)
+                              locs))
+             (incf start-row)))
+    (reverse locs)))
+
+;;  GET-END-TILE
+;; -----------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board
+;;          TILE, a TILE on the board
+;;          D-HORIZ & D-VERT, how many spaces to check in each direction (+/-1)
+;;  OUTPUT: the first or last TILE in a row of tiles
+
+(defun get-end-tile (board tile d-horiz d-vert)
+  (let ((prev-row (tile-row tile))
+        (prev-col (tile-col tile))
+        (row (tile-row tile))
+        (col (tile-col tile)))
+    (while (not (or (off-board? board row col)
+                    (empty-space? board row col)))
+           (setf prev-row row)
+           (setf prev-col col)
+           (setf row (+ row d-vert))
+           (setf col (+ col d-horiz)))
+    (tile-from-loc board prev-row prev-col)))
+
+;;  WORD-TILES-FROM-START-END
+;; -------------------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board
+;;          START, a lis
+
+;;  OFF-BOARD?
+;; ------------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board
+;;          ROW, COL, a possible tile position
+;;  OUTPUT: T if (ROW, COL) is off the board, NIL otherwise
+
+(defun off-board? (board row col)
+  (let ((dims (array-dimensions board)))
+    (or (< row 0)
+        (< col 0)
+        (> row (first dims))
+        (> col (second dims)))))
+
+;;  EMPTY-SPACE?
+;; -------------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board
+;;          ROW, COl, a possible tile position
+;;  OUTPUT: T if (ROW, COL) is empty in GAME, NIL otherwise
+
+(defun empty-space? (board row col)
+  (let ((loc (aref board row col)))
+    (or (equal loc *open*)
+        (equal loc *tw*)
+        (equal loc *dl*)
+        (equal loc *dw*)
+        (equal loc *tl*))))
+
+;;  TILE-FROM-LOC
+;; ------------------------
+;;  INPUTS: BOARD, a 2D array representing a SCRABBLE board
+;;          ROW & COL, row and col of the tile
+;;  OUTPUT: The TILE at the given position on BOARD
+
+(defun tile-from-loc (board row col)
+  (if (empty-space? board row col)
+    nil
+    (aref board row col)))
+
+;;  PLACE-TILE!
+;; ----------------------
+;;  INPUTS: BOARD, a 2D array representing a scrabble board
+;;          TILE, a TILE struct
+;;          ROW & COL, the location on the board to place the tile
+;;  OUTPUTS: The modified tile
+;;  SIDE-EFFECTS: Modifies the tile to contain it's location,
+;;                and modifies the board to contain the tile
+
+(defun place-tile! (board tile row col)
+  (setf (tile-row tile) row)
+  (setf (tile-col tile) col)
+  (setf (aref board row col) tile)
+  tile)
+
+
+;;  REMOVE-FROM-RACK! 
+;; ------------------------
+;;  INPUT: GAME, a SCRABBLE struct, LETTER, a letter
+;;  OUTPUT: A tile of that LETTER 
+;;  SIDE EFFECT: Modifies GAME by removing tile from the rack
+
+(defun remove-from-rack! (game letter)
+  (let ((player (whose-turn game))
+        (tile (make-tile :letter letter
+                         :value (svref *letter-val-array*
+                                       (position letter *letters-array*)))))
+    (if (equal player *ply0*)	
+      (setf (scrabble-rack_0 game) 
+            (remove tile (scrabble-rack_0 game) :test #'tile-eq? :count 1))
+      (setf (scrabble-rack_1 game)
+            (remove tile (scrabble-rack_1 game) :test #'tile-eq? :count 1)))
+    tile))
+
+;;  REFILL-RACKS!
+;; -----------------------
+;;  INPUTS: GAME, a SCRABBLE struct
+;;  OUTPUT: The modified GAME (for convenience)
+;;  SIDE-EFFECT: Refills the racks for both players in GAME
+
+(defun refill-racks! (game)
+  ;; Player 0
+  (pick-tiles! game *ply0* (- 7 (length (scrabble-rack_0 game))))
+  ;; Player 1
+  (pick-tiles! game *ply1* (- 7 (length (scrabble-rack_1 game)))))
+
+;;  PICK-TILES!
+;; ------------------------
+;;  INPUT: GAME, scrabble struct,
+;;         PLAYER, either *ply0* or *ply1*,
+;;         N, number
+;;  OUTPUT: GAME, modified such that N tiles are added to PLAYER's rack
+;;  SIDE-EFFECT: Modifies the appropriate rack in GAME
+
+(defun pick-tiles! (game player n)
+  (dotimes (i n)
+    (if (equal player *ply0*)
+      ;; Player 0
+      (setf (scrabble-rack_0 game)
+            (cons (first (scrabble-bag game))
+                  (scrabble-rack_0 game)))
+      ;; Player 1
+      (setf (scrabble-rack_1 game)
+            (cons (first (scrabble-bag game))
+                  (scrabble-rack_1 game))))
+    ;; Remove tile from bag
+    (setf (scrabble-bag game)
+          (rest (scrabble-bag game)))))
+
