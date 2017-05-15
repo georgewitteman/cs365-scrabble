531bc1676a5f9862c32d8ab25bd7dcaa93ff35d1
George Witteman <georgewitteman@gmail.com>
2017-05-15 16:14:28 -0400

    Update file-read.lisp

--- file-read.lisp
+++ file-read.lisp
@@ -1,7 +1,17 @@
+;; FILE-TO-ARRAY
+;; ---------------------------
+;; INPUTS: FILENAME, the name of a file as a STRING
+;; OUTPUT: An array where each line of the array is an element in FILENAME
+
 (defun file-to-array (filename)
   (let ((file-as-list (file-to-list filename)))
     (make-array (length file-as-list)
                 :initial-contents file-as-list)))
+
+;; FILE-TO-LIST
+;; ---------------------------
+;; INPUTS: FILENAME, the name of a file as a STRING
+;; OUTPUT: A list where each element is a line in FILENAME
 
 (defun file-to-list (filename)
   (with-open-file (stream filename)
